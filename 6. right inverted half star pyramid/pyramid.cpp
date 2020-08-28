7ccd2dd522d32d07db0aca7f81bad69edbc82016
SaroarShahan <shahanntk@gmail.com>
2020-08-29 00:28:30 +0600

    done right inverted half pyramid

---
+++ 6. right inverted half star pyramid/pyramid.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for(int row = n; row >= 1; row--) {
+        for(int space = 1; space <= n - row; space++)
+            cout << " ";
+        for(int col = 1; col <= row; col++)
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
+
