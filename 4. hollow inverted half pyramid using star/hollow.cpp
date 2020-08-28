aa9ce7951d5e1a633734ced0d7f2907a0c0479a9
SaroarShahan <shahanntk@gmail.com>
2020-08-29 00:17:55 +0600

    done hollow inverted half pyramid

---
+++ 4. hollow inverted half pyramid using star/hollow.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for(int row = n; row > 0; row--) {
+        for(int col = 1; col <= row; col++) {
+            if(col == 1 || row == n || row == col)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
