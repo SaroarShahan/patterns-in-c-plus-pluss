c5edfbce1077e398c11b470f1552673c2a277780
SaroarShahan <shahanntk@gmail.com>
2020-08-29 00:31:04 +0600

    done right hollow half pyramid

---
+++ 7. right hollow half star pyramid/pyramid.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for(int row = 1; row <= n; row++) {
+        for(int space = 1; space <= n - row; space++)
+            cout << " ";
+        for(int col = 1; col <= row; col++) {
+            if(row == n || col == 1 || row == col)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
