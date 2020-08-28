ad4793109761d164b5fc6f8a29ccd1dc846c65b8
SaroarShahan <shahanntk@gmail.com>
2020-08-29 00:27:13 +0600

    done right half pyramid

---
+++ 5. right half star pyramid/pyramid.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for(int row = 1; row <= n; row++) {
+        for(int space = 1; space <= n - row; space++)
+            cout << " ";
+        for(int col = 1; col <= row; col++)
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
